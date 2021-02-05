8921ea2a3d5c9c15eee33e213d494fcd61d6e23c
12px <d@12px.io>
2021-02-05 16:54:01 -0700

    Add DM command to associate a guild.

---
+++ discord/commands/server/dm.js
@@ -0,0 +1,23 @@
+// dm.js - cache and DM a user
+
+module.exports = {
+  name: 'dm',
+  gate: 0,
+  auth: true,
+
+  help: {
+    head: "~/dm",
+    desc: [
+      "Sends you a DM to use commands via the server."
+    ]
+  },
+
+  info: {
+    title: "Hello From {guild.name}!",
+    description: "Any commands used in this DM will execute via {guild.name}"
+  },
+
+  fire: async function (Anni, Msg) {
+    Anni.Reply(Msg, this.info).dm()
+  }
+}
