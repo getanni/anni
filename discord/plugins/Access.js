97cf2cdc67659065434c37b8062d7f99a5a0ffcf
12px <d@12px.io>
2021-02-12 20:37:41 -0700

    Add employ checks to Access

--- discord/plugins/Access.js
+++ discord/plugins/Access.js
@@ -3,30 +3,33 @@
 module.exports = Anni => {
   Anni.Access = {
     Get: function (Anni, Msg) {
-      let card = this.badge(1)
+      let card = this.badge(1), mods = Msg.employ
       let auth = Msg.guild || Anni.Cache.server(Msg.author.id)
       let user = auth ? auth.members.cache.get(Msg.author.id) : Msg.member
 
       if (user && auth) {
-        if (this.staff(user, auth)) card = this.badge(3)
-        if (this.admin(user))       card = this.badge(5)
-        if (this.owner(user, auth)) card = this.badge(7)
-        if (this.maker(user))       card = this.badge(9)
+        if (this.staff(user, auth, mods)) card = this.badge(3)
+        if (this.admin(user))             card = this.badge(5)
+        if (this.owner(user, auth))       card = this.badge(7)
+        if (this.maker(user))             card = this.badge(9)
       }
 
       return { ...card, auth }
     },
 
-    forge: function (gate, auth) { return { ...this.badge(gate), auth } },
-    badge: function (gate) { return this.$badges[gate] },
-    maker: function (user) { return Anni.bot.owners.includes(user.id) },
+    forge: function (gate, auth) {  return { ...this.badge(gate), auth } },
+    badge: function (gate) {        return this.$badges[gate] },
+    maker: function (user) {        return Anni.bot.owners.includes(user.id) },
     owner: function (user, guild) { return user.id == guild.ownerID },
-    admin: function (user) { return user.hasPermission('ADMINISTRATOR') },
-    staff: function (user, guild) {
-      let list = ['mod', 'mods', 'moderator', 'moderators', 'staff']
-      let _inc = role => list.includes(role.name.toLowerCase())
-      let role = guild.roles.cache.find(_inc)
-      return role && user.roles.cache.get(role.id) ? true : false
+    admin: function (user) {        return user.hasPermission('ADMINISTRATOR') },
+    staff: function (user, guild, mods) {
+      // check for a generic mod role
+      let check = role => this.$basic.includes(role.name.toLowerCase())
+      let basic = guild.roles.cache.find(check)
+      if (basic && user.roles.cache.get(basic.id)) return true
+      // check for a server-defined mod role
+      for (let id of mods) if (user.roles.cache.get(id)) return true
+      return false
     },
 
     $badges: {
@@ -38,6 +41,7 @@ module.exports = Anni => {
       5: { level: 5, name: 'Admin' },
       7: { level: 7, name: 'Owner' },
       9: { level: 9, name: 'Author' }
-    }
+    },
+    $basic: [ 'mod', 'mods', 'moderator', 'moderators', 'staff' ]
   }
 }
