b44a0810d4bda348001404ab32520bc60d1e50eb
12px <d@12px.io>
2021-02-12 22:10:49 -0700

    Finish list-roeles.js pseudo code

---
+++ discord/commands/general/list-roles.js
@@ -0,0 +1,25 @@
+// list-times.js - returns current time in the server
+
+module.exports = {
+  name: "roles",
+  auth: true,
+  gate: 4,
+
+  help: {
+    head: "~/roles",
+    desc: "Lists the roles and IDs in the server."
+  },
+
+  post: { 
+    head: "Roles in {guild.name}",
+    desc: []
+  },
+
+  fire: async function (Anni, Msg) {
+    let post = Anni.$Copy(this.post)
+    await Msg.auth.roles.cache.each(role => {
+      post.desc.push(`\`${role.id}\`<@&${role.id}>`)
+    })
+    return Anni.Reply(Msg, post).dm()
+  }
+}
