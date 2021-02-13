a3567b1156e2e0da55a513c944b76ac7cc77b00d
12px <d@12px.io>
2021-02-12 22:09:41 -0700

    Finish bot-staff.js pseudo-code.

--- discord/commands/configs/bot-staff.js
+++ discord/commands/configs/bot-staff.js
@@ -1,4 +1,4 @@
-// bday-channel.js - sets the channel for birthday reminders
+// bot-staff.js - list and toggle staff roles
 
 module.exports = {
   name: "mods",
@@ -19,7 +19,7 @@ module.exports = {
   lang: {
     add: "Added <@&{role}> as a staff role.",
     rem: "Removed <@&{role}> as a staff role.",
-    err: "Couldn't find the role `{role}` in **{guild.name}**."
+    err: "Couldn't find the role `{msg}` in **{guild.name}**."
   },
 
   post: {
@@ -28,13 +28,31 @@ module.exports = {
   },
 
   fire: async function (Anni, Msg) {
-    // if no arguments, list current mod roles
-    let basic = Anni.Access.$basic(Msg.auth)
-    let staff = basic ? [ basic, ...Msg.employ ] : Msg.employ
+    if (!Msg.args.length) {
+      // if no arguments, list current mod roles
+      let basic = Anni.Access.$basic(Msg.auth)
+      let staff = basic ? [ basic, ...Msg.employ ] : Msg.employ
 
-    let post = { head: this.lang.head, desc: [] }
-    post.desc = `<@&${staff.join('>, <@&')}>`
+      let post = { head: this.lang.head, desc: [] }
+      post.desc = `<@&${staff.join('>, <@&')}>`
 
-    return Anni.Reply(Msg, post).dm()
+      return Anni.Reply(Msg, post).dm()
+    } else {
+      // else toggle the roles a staff role
+      let role = Anni.Str.strip(Msg.full)
+      let byID = role => role.id == role
+      let data = Msg.auth.roles.cache.find(byID)
+      // return if the role doesn't exist
+      if (!data) return Anni.Reply(Msg, this.lang.err).send()
+      let index = Msg.employ.indexOf(role)
+      // add a non-staff role, remove a staff role
+      if (index < 0) Msg.employ.push(role)
+      else Msg.employ.splice(index, 1)
+      let employ = JSON.stringify(Msg.employ)
+      await Anni.$Configs.set(Msg.auth.id, { employ })
+      
+      let message = index < 0 ? this.lang.add : this.lang.rem
+      return Anni.Reply(Msg, message, { role }).send()
+    }
   }
 }
